cddf98e1b42b2d52bc49c4ffd35f0fbc3592505f
Cyberstunna <nennii@fomosocial.ca>
2023-09-11 20:43:37 -0600

    models and data for View generated

--- xeroTax/Controllers/HomeController.cs
+++ xeroTax/Controllers/HomeController.cs
@@ -2,8 +2,6 @@
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
 using xeroTax.Models;
-// using System.Collections.Generic;
-// using System.Linq;
 
 namespace xeroTax.Controllers;
 
@@ -27,19 +25,39 @@ public class HomeController : Controller
                 Id = Guid.Parse(x.Element("Id")!.Value),
                 SubmittedDateTimeUtc = DateTime.Parse(x.Element("SubmittedDateTimeUtc")!.Value),
                 ProductName = x.Element("ProductName")?.Value,
-                // Add the rest of the properties here
-                Machine = new MachineInfo
-                {
-                    // Populate MachineInfo properties
-                }
+                ProductVersion = x.Element("ProductVersion")?.Value,
+                Type = x.Element("Type")?.Value,
+                Email = x.Element("Email")?.Value,
+                Exception = x.Element("Exception")?.Value,
+                ExceptionType = x.Element("ExceptionType")?.Value,
+                ExceptionSource = x.Element("ExceptionSource")?.Value,
+                ExceptionDetails = x.Element("ExceptionDetails")?.Value,
             })
             .ToList();
 
         // Get Values for Bar Chart for Version Number
+        var versionExceptionCounts = xmlFeedbackList?
+            .GroupBy(x => x.ProductVersion!)
+            .ToDictionary(
+                group => group.Key,
+                group => group.Count()
+            );
 
         // Get Values for Pie Chart for Exception Source
+        var sourceExceptionCounts = xmlFeedbackList?
+            .GroupBy(x => x.ExceptionSource!)
+            .ToDictionary(
+                group => group.Key,
+                group => group.Count()
+            );
+    
+        var modelData = new HomeModel {
+            xmlFeedback = xmlFeedbackList,
+            versionNumberChart = versionExceptionCounts,
+            exceptionSourceChart = sourceExceptionCounts,
+        };
 
-        return View(xmlFeedbackList);
+        return View(modelData);
     }
 
     public IActionResult Privacy()
---
+++ xeroTax/Models/HomeModel.cs
@@ -0,0 +1,21 @@
+namespace xeroTax.Models;
+
+public class HomeModel {
+    public List<XmlFeedback>? xmlFeedback;
+
+    public Dictionary<string, int>? versionNumberChart;
+
+    public Dictionary<string, int>? exceptionSourceChart;
+}
+
+// public class VersionNumberChart {
+//     public string? versionNumber;
+
+//     public int count;
+// }
+
+// public class ExceptionSourceChart {
+//     public string? exceptionSource;
+
+//     public int count;
+// }
